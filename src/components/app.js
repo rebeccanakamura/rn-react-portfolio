7f32de6e1d669f4094fc6fc9df0c747206f72857
rebeccanakamura <rebeccagithub@gmail.com>
2019-10-02 15:27:26 -0600

    update

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio-container'
+
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h2>Rebecca Nakamura Portfolio</h2>
+        <PortfolioContainer />
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
